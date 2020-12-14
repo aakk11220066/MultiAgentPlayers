de29a7d7af60c54068b9479e5b8ad469f6a0cf7b
asaelavia <asaelavia@gmail.com>
2020-12-14 15:22:54 +0200

    Minimax implementation

--- intro_to_AI_hw2_2020-provided-code/SearchAlgos.py
+++ intro_to_AI_hw2_2020-provided-code/SearchAlgos.py
@@ -1,11 +1,14 @@
 """Search Algos: MiniMax, AlphaBeta
 """
 from utils import ALPHA_VALUE_INIT, BETA_VALUE_INIT
-#TODO: you can import more modules, if needed
+
+# TODO: you can import more modules, if needed
+import numpy as np
+from utils import get_directions
 
 
 class SearchAlgos:
-    def __init__(self, utility, succ, perform_move, goal):
+    def __init__(self, utility, succ, perform_move, goal=None):
         """The constructor for all the search algos.
         You can code these functions as you like to, 
         and use them in MiniMax and AlphaBeta algos as learned in class
@@ -31,7 +34,31 @@ class MiniMax(SearchAlgos):
         :param maximizing_player: Whether this is a max node (True) or a min node (False).
         :return: A tuple: (The min max algorithm value, The direction in case of max node or None in min mode)
         """
-        #TODO: erase the following line and implement this function.
+        succs = self.succ(state)
+        if succs is None:
+            return (self.utility(state, False), (0, 0))
+        if depth < 0:
+            return (self.utility(state, True), (0, 0))
+        if maximizing_player:
+            max_score = (-np.inf, (0, 0))
+            for succ in succs:
+                index = 0
+                res = self.search(succ, depth - 1, False)
+                if res[0] > max_score[0]:
+                    max_score = (res[0], get_directions()[index])
+                index += 1
+            return max_score
+        else:
+            min_score = (np.inf, (0, 0))
+            for succ in succs:
+                index = 0
+                res = self.search(succ, depth - 1, True)
+                if res[0] < min_score[0]:
+                    min_score = (res[0], None)
+                index += 1
+            return min_score
+
+        # TODO: erase the following line and implement this function.
         raise NotImplementedError
 
 
@@ -46,5 +73,5 @@ class AlphaBeta(SearchAlgos):
         :param: beta: beta value
         :return: A tuple: (The min max algorithm value, The direction in case of max node or None in min mode)
         """
-        #TODO: erase the following line and implement this function.
+        # TODO: erase the following line and implement this function.
         raise NotImplementedError
--- intro_to_AI_hw2_2020-provided-code/players/MinimaxPlayer.py
+++ intro_to_AI_hw2_2020-provided-code/players/MinimaxPlayer.py
@@ -2,14 +2,23 @@
 MiniMax Player
 """
 from players.AbstractPlayer import AbstractPlayer
-#TODO: you can import more modules, if needed
+from SearchAlgos import MiniMax
+
+
+# TODO: you can import more modules, if needed
 
 
 class Player(AbstractPlayer):
     def __init__(self, game_time, penalty_score):
-        AbstractPlayer.__init__(self, game_time, penalty_score) # keep the inheritance of the parent's (AbstractPlayer) __init__()
-        #TODO: initialize more fields, if needed, and the Minimax algorithm from SearchAlgos.py
-
+        AbstractPlayer.__init__(self, game_time,
+                                penalty_score)  # keep the inheritance of the parent's (AbstractPlayer) __init__()
+        # TODO: initialize more fields, if needed, and the Minimax algorithm from SearchAlgos.py
+        self.greys = set()
+        self.my_loc = (-1, -1)
+        self.opp_loc = (-1, -1)
+        self.fruits = {}
+        self.board = []
+        self.minimax = MiniMax(self.utility, self.succ, self.perform_move)
 
     def set_game_params(self, board):
         """Set the game parameters needed for this player.
@@ -19,8 +28,22 @@ class Player(AbstractPlayer):
             - board: np.array, a 2D matrix of the board.
         No output is expected.
         """
-        #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        self.board = board
+        # Get squares classes
+        row_index = 0
+        col_index = 0
+        for row in board:
+            for col in row:
+                if col == 0:
+                    continue
+                elif col == -1:
+                    self.greys.add((row_index, col_index))
+                elif col == 1:
+                    self.my_loc = (row_index, col_index)
+                elif col == 2:
+                    self.opp_loc = (row_index, col_index)
+                else:
+                    self.fruits[(row_index, col_index)] = col
 
     def make_move(self, time_limit, players_score):
         """Make move with this Player.
@@ -29,9 +52,9 @@ class Player(AbstractPlayer):
         output:
             - direction: tuple, specifing the Player's movement, chosen from self.directions
         """
-        #TODO: erase the following line and implement this function.
-        raise NotImplementedError
+        # TODO: erase the following line and implement this function.
 
+        raise NotImplementedError
 
     def set_rival_move(self, pos):
         """Update your info, given the new position of the rival.
@@ -39,9 +62,12 @@ class Player(AbstractPlayer):
             - pos: tuple, the new position of the rival.
         No output is expected
         """
-        #TODO: erase the following line and implement this function.
+        # TODO: erase the following line and implement this function.
+        self.greys.add(pos)
+        self.opp_loc = pos
+        if pos in self.fruits.keys():
+            self.fruits.pop(pos)
         raise NotImplementedError
-
 
     def update_fruits(self, fruits_on_board_dict):
         """Update your info on the current fruits on board (if needed).
@@ -51,13 +77,26 @@ class Player(AbstractPlayer):
                                     'value' is the value of this fruit.
         No output is expected.
         """
-        #TODO: erase the following line and implement this function. In case you choose not to use it, use 'pass' instead of the following line.
-        raise NotImplementedError
-
+        # TODO: erase the following line and implement this function. In case you choose not to use it, use 'pass' instead of the following line.
+        pass
 
     ########## helper functions in class ##########
-    #TODO: add here helper functions in class, if needed
-
+    # TODO: add here helper functions in class, if needed
 
     ########## helper functions for MiniMax algorithm ##########
-    #TODO: add here the utility, succ, and perform_move functions used in MiniMax algorithm
+    # TODO: add here the utility, succ, and perform_move functions used in MiniMax algorithm
+    def utility(self, state, heuristics):
+        if heuristics:
+            return self.hueristic(state)
+        pass
+
+    def succ(self, state):
+
+        pass
+
+    def perform_move(self, state, maximizing_player):
+
+        pass
+
+    def hueristic(self, state):
+        pass
